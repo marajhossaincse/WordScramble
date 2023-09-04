9351ec8a6ca5acd7831a9d9333e21d6cdc6e9a2b
Maraj Hossain <marajhossaincse@gmail.com>
2023-09-05 02:43:11 +0600

    initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Maraj Hossain on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
