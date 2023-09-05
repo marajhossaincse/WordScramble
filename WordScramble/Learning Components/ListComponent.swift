b6cdc5e21ba207370d12c3645e6f21f0215c7d96
Maraj Hossain <marajhossaincse@gmail.com>
2023-09-06 02:08:47 +0600

    further r&d on list component

--- WordScramble/Learning Components/ListComponent.swift
+++ WordScramble/Learning Components/ListComponent.swift
@@ -8,29 +8,33 @@
 import SwiftUI
 
 struct ListComponent: View {
+    var people = ["Finn", "Leia", "Luke", "Rey"]
+
     var body: some View {
-        List {
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-
-            Section("Section 2") {
-                ForEach(0 ..< 5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-
-            Section("Section 3") {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
+        List(people, id: \.self) {
+            Text("\($0)")
+//            Section("Section 1") {
+//                Text("Static row 1")
+//                Text("Static row 2")
+//            }
+//
+//            Section("Section 2") {
+//                ForEach(0 ..< 5) {
+//                    Text("Dynamic row \($0)")
+//                }
+//            }
+//
+//            Section("Section 3") {
+//                Text("Static row 3")
+//                Text("Static row 4")
+//            }
 
 //            Text("Hello World!")
 //            Text("Hello World!")
 //            Text("Hello World!")
 //            Text("Hello World!")
         }
+        .listStyle(.grouped)
     }
 }
 
